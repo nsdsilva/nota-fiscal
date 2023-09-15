1daedc0230376fb8ff4353092bd3d3c05d479180
eliseurm <eliseu@sonner.com.br>
2023-09-15 09:38:13 -0300

    inicio

--- src/main/java/br/com/projetonotafiscal/notafiscal/Infra/ValidacaoException.java
+++
@@ -1,8 +0,0 @@
-package br.com.projetonotafiscal.notafiscal.Infra;
-
-public class ValidacaoException extends RuntimeException {
-
-    public ValidacaoException(String mensagem) {
-        super(mensagem);
-    }
-}
